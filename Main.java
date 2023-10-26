6d78659af587cbe94cd1f231a92bc702c6ab67c1
RoaaAbeery <148685929+RoaaAbeery@users.noreply.github.com>
2023-10-26 15:06:49 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,202 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//        Q1
+//        Scanner s1=new Scanner(System.in);
+//        System.out.printf("input first number :");
+//        int num1=s1.nextInt();
+//        System.out.printf("input first number :");
+//        int num2=s1.nextInt();
+//        System.out.println("125 + 24 = " +num1+num2);
+//        System.out.println("125 - 24 " + num1-num2);
+//        System.out.println("125 * 24 = " +num1*num2);
+//        System.out.println("125 / 24 = " +num1/num2);
+//        System.out.println("125 % 24 = " +num1%num2);
+//Q2
+//        Scanner s2=new Scanner(System.in);
+//        System.out.println("input a number :");
+//        int num1=s2.nextInt();
+//int mul =0;
+//        for (int i = 1; i <=10 ; i++) {
+//             mul=num1*i;
+//
+//
+//            System.out.println(num1 + " * " + i +"="+ mul);
+//
+//        }
+//        Q3
+
+//Scanner s3=new Scanner(System.in);
+//        System.out.println("Radius=");
+//        double r=s3.nextDouble();
+//        double PI =3.14;
+//        double perimeter=2*PI*r;
+//        double circle =PI*r*r;
+//        System.out.println("Perimeter is = " + perimeter);
+//        System.out.println("Circle is = " + circle);
+//
+//Q4
+
+//Scanner s4 = new Scanner(System.in);
+//        System.out.println("Enter the count of numbers:");
+//   int c = s4.nextInt();
+//        int ave=0;
+//        for (int i = 1; i ==c ; i++) {
+//            System.out.println("Enter an integer");
+//            int num = s4.nextInt();
+//            ave=+num;
+//        }
+//
+//        System.out.println(ave/c);
+
+//Q5
+//       Scanner s2=new Scanner (System.in);
+//        System.out.println("Input the 1st number:");
+//        int number1 = s2.nextInt();
+//        System.out.println("Input the 2st number:");
+//        int number2 = s2.nextInt();
+//        System.out.println("Input the 3st number:");
+//        int number3 = s2.nextInt();
+//
+//        if( number1+number2 == number3)
+//            System.out.println( "Ture" );
+//
+//        else if (number1+number2 != number3)
+//            System.out.println("false");
+//
+//        else
+//            System.out.println("Thank you");
+//Q6
+//        String str, rev = "";
+//      Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter a string:");
+//        str = sc.nextLine();
+//
+//       int length = str.length();
+//
+//       for ( int i = length - 1; i >= 0; i-- )
+//          rev = rev + str.charAt(i);
+//        System.out.println(rev);
+//Q7
+//        Scanner r=new Scanner(System.in);
+//     System.out.println("enter the number : ");
+//       int number=r.nextInt();
+//
+//      if (number%2==0){
+//          System.out.println("number is even");
+//
+//        } else{
+//            System.out.println("number is odd");
+//
+//
+//        }
+//        Q8
+//        Scanner s3 = new Scanner(System.in);
+//
+//        System.out.print("Enter temperature in Centigrade: ");
+//        double centigrade= s3.nextDouble();
+//        double fahrenheit=(centigrade*1.8)+32;
+//
+//
+//        System.out.println("Temperature in Fahrenheit is: " + fahrenheit);
+
+//Q9
+//        Scanner s9 =new Scanner(System.in);
+//        System.out.println("Input a string:");
+//        String st=s9.nextLine();
+//        System.out.println("Input a number:");
+//        int num = s9.nextInt();
+//        System.out.println(st.charAt(num));
+//Q10
+//        Scanner s3=new Scanner(System.in);
+//        System.out.println("Enter the width");
+//        double w=s3.nextDouble();
+//        System.out.println("Enter the height");
+//        double h =s3.nextDouble();
+//        double area =w*h;
+//        double perimeter=2*(w+h);
+//        System.out.println("Area is = " + area);
+//        System.out.println("Perimeter is = " + perimeter);
+//Q11
+//        Scanner s11=new Scanner(System.in);
+//        System.out.println("Input first integer:");
+//        int num1=s11.nextInt();
+//        System.out.println("Input first integer:");
+//        int num2=s11.nextInt();
+//        if (num1!=num2){
+//            System.out.println(num1+"!="+num2);
+//        }  if (num1<num2) {
+//            System.out.println(num1+"<"+num2);
+//
+//        } if (num1<=num2) {
+//            System.out.printf(num1+"<="+num2);
+//        }
+//Q12
+//        Scanner s12 = new Scanner(System.in);
+//        System.out.println("Input first number :");
+//        int num1 = s12.nextInt();
+//        System.out.println("Input second number :");
+//        int num2 = s12.nextInt();
+//        System.out.println("Input third number :");
+//        int num3 = s12.nextInt();
+//        System.out.println("Input fourth number :");
+//        int num4 = s12.nextInt();
+//
+//Q13
+//        if (num1 == num2 && num2 == num3 && num3 == num4) {
+//            System.out.println("Numbers are equal");
+//        }
+//        else System.out.println("Numbers are not equal");
+//    }
+//       Q14
+//     Scanner r=new Scanner(System.in);
+//        System.out.println("Input a number : ");
+// int number=r.nextInt();
+//
+// if (number>0){
+//     System.out.println("Number is Positive");
+//
+// } else if (number<0) {
+//     System.out.println("Number is Negative");
+// }
+//else {
+//     System.out.println("Number is Zero ");
+//        }
+//Q15
+//        Scanner s=new Scanner(System.in);
+//
+//
+//
+//        int pcount=0;
+//        int ncount=0;
+//        int zcount=0;
+//        System.out.println("please enter the number and -1 to quit");
+//        int number;
+//        do{
+//            System.out.println("please enter the number ");
+//            number = s.nextInt();
+//        if (number==-1) {
+//           break;}
+//
+//            if (number > 0) {
+//
+//                pcount++;
+//            }
+//            if (number < 0) {
+//
+//                ncount++;
+//            } else{
+//            zcount++;
+//        }}
+//while(true);
+//        System.out.println(pcount+"Positive");
+//        System.out.println(ncount+"negative");
+//        System.out.println(zcount+"zero");
+
+
+
+}}
